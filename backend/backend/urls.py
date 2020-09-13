8efa953728d143f1e073aa0b7c7dc76eb2d77ee4
AtilaMedeiros <atila.medeiros@gmail.com>
2020-09-13 20:55:43 -0300

    creating serializers, views, urls to endpoint list into core

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,11 +2,13 @@ from rest_framework import routers
 from django.urls import include, path
 from django.contrib import admin
 from .views import UserViewSet, GroupViewSet
+from core.views import ListViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
+router.register(r'list', ListViewSet)
 
 
 urlpatterns = [
