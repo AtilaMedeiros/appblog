5fa9406d3f47d0c47b9b36bfc88333d3f98bc5bb
AtilaMedeiros <atila.medeiros@gmail.com>
2020-09-13 21:44:47 -0300

    finishing backend with nested serializers of the list

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,13 +2,14 @@ from rest_framework import routers
 from django.urls import include, path
 from django.contrib import admin
 from .views import UserViewSet, GroupViewSet
-from core.views import ListViewSet
+from core.views import ListViewSet, ItemViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
 router.register(r'list', ListViewSet)
+router.register(r'item', ItemViewSet)
 
 
 urlpatterns = [
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,10 +1,17 @@
 from rest_framework import viewsets, permissions
-from .serializers import ListSerializer
-from .models import List
+from .serializers import ListSerializer, ItemSerializer
+from .models import List, Item
 
 
 class ListViewSet(viewsets.ModelViewSet):
 
     queryset = List.objects.all()
     serializer_class = ListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
     permission_classes = [permissions.IsAuthenticated]
